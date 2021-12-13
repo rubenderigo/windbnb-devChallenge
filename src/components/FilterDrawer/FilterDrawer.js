444808a114098f9d475236299aaa14a8383a737d
rubenderigo <rm.derigo@gmail.com>
2021-12-13 19:37:58 -0300

    add close drawer icon

--- src/components/FilterDrawer/FilterDrawer.js
+++ src/components/FilterDrawer/FilterDrawer.js
@@ -1,23 +1,24 @@
-import { useState, useRef } from 'react';
-import FormSearch from 'components/FilterForm/FilterForm';
-import SelectLocations from 'components/SelectLocations/SelectLocations';
-import SelectGuests from 'components/SelectGuests/SelectGuests';
-import styles from './FilterDrawer.module.css';
-import useClickAway from 'hooks/clickAway';
+import { useState, useRef } from "react";
+import FormSearch from "components/FilterForm/FilterForm";
+import SelectLocations from "components/SelectLocations/SelectLocations";
+import SelectGuests from "components/SelectGuests/SelectGuests";
+import styles from "./FilterDrawer.module.css";
+import useClickAway from "hooks/clickAway";
 
 const FilterDrawer = ({ closeDrawer }) => {
   const [showLocations, setShowLocations] = useState(false);
   const [showGuests, setShowGuests] = useState(false);
   const filterDrawer = useRef();
   useClickAway({ ref: filterDrawer, handleClick: closeDrawer });
-  
+
   return (
-    <div className={styles['drawer-container']}>
-      <div className={styles['drawer']} ref={filterDrawer}>
+    <div className={styles["drawer-container"]}>
+      <div className={styles["drawer"]} ref={filterDrawer}>
+        <span className={`material-icons ${styles["close-drawer"]}`} onClick={closeDrawer}>close</span>
         <div>
           <FormSearch
             showSearch
-            showLabel 
+            showLabel
             classLabel="label"
             classButton="button-secondary"
             classForm="form-filter-secondary"
@@ -28,11 +29,11 @@ const FilterDrawer = ({ closeDrawer }) => {
             setShowLocations={setShowLocations}
           />
         </div>
-        <div className={styles['location-guests-container']}>
-          <div className={styles['location']}>
+        <div className={styles["location-guests-container"]}>
+          <div className={styles["location"]}>
             {showLocations && <SelectLocations />}
           </div>
-          <div className={styles['guests']}>
+          <div className={styles["guests"]}>
             {showGuests && <SelectGuests />}
           </div>
         </div>
